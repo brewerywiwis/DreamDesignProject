697ddfffad59c28f0e1659d57437d6f29cb6e91d
cchaiyatad <chaiyatad@gmail.com>
2020-11-14 21:29:45 +0700

    Add auto increment and pk

--- sqlCode/createTable.sql
+++ sqlCode/createTable.sql
@@ -1,57 +1,66 @@
 CREATE TABLE IF NOT EXISTS user(
-   uid VARCHAR(15),
+   uid BIGINT,
    username VARCHAR(20),
-   password VARCHAR(20)
+   password VARCHAR(20),
+   PRIMARY KEY (uid)
 );
 
 CREATE TABLE IF NOT EXISTS designer(
-   uid VARCHAR(15),
+   uid BIGINT,
    bio VARCHAR(100),
-   reviewScore FLOAT
+   reviewScore FLOAT,
+   PRIMARY KEY (uid)
 );
 
 CREATE TABLE IF NOT EXISTS customer(
-   uid VARCHAR(15)
+   uid BIGINT,
+   PRIMARY KEY (uid)
 );
 
 CREATE TABLE IF NOT EXISTS admin(
-   uid VARCHAR(15)
+   uid BIGINT,
+   PRIMARY KEY (uid)
 );
 
 CREATE TABLE IF NOT EXISTS contract(
-    ctid VARCHAR(15),
+    ctid BIGINT NOT NULL AUTO_INCREMENT,
     detail VARCHAR(300),
-    mid VARCHAR(15)
+    mid VARCHAR(15),
+    PRIMARY KEY (ctid)
 );
 
 CREATE TABLE IF NOT EXISTS jobposting(
-    jid VARCHAR(15),
+    jid BIGINT NOT NULL AUTO_INCREMENT,
     did VARCHAR(15),
     sampleWorks BLOB,
-    description VARCHAR(500)
+    description VARCHAR(500),
+    PRIMARY KEY (jid)
 );
 
 CREATE TABLE IF NOT EXISTS transaction(
-    tid VARCHAR(15),
+    tid BIGINT NOT NULL AUTO_INCREMENT,
     bank VARCHAR(50),
     amount FLOAT,
     status BOOLEAN,
-    paymentMethod VARCHAR(50)
+    paymentMethod VARCHAR(50),
+    PRIMARY KEY (tid)
 );
 
 CREATE TABLE IF NOT EXISTS advertisement(
-    aid VARCHAR(15),
+    aid BIGINT NOT NULL AUTO_INCREMENT,
     uid VARCHAR(15),
     startDate DATE,
     endDate DATE,
     advertiserEmail VARCHAR(100),
-    adPicture BLOB
+    adPicture BLOB,
+    PRIMARY KEY (aid)
 );
 
 CREATE TABLE IF NOT EXISTS matchs(
-    mid VARCHAR(15),
+    mid BIGINT NOT NULL AUTO_INCREMENT,
     did VARCHAR(15),
     cid VARCHAR(15),
     tid VARCHAR(15),
-    score FLOAT
+    score FLOAT,
+    PRIMARY KEY (mid)
 );
--- sqlCode/sampleData.sql
+++ sqlCode/sampleData.sql
@@ -1,9 +1,9 @@
 INSERT INTO Contract VALUES 
 (1000000001, '1. designer will design 1 brochure  for 1000 baht 2. designer will finish the work within 3 days 3. no more than 2 redesign 4. customer will receive .JPG, .PDF and .AI files', 2000000001), 
-(1000000002, '1. designer will design 3 banners for 1500 baht 2. designer will finish the work within 3 days 3. no more than 3 redesign 4. customer will receive .JPG and .PNG files 5. additional 100 baht for each .AI file', 2000000002), 
-(1000000003, '1. designer will design 1 banner for 600 baht 2. designer will finish the work within 2 days 3. 2 times free redesign. if redesign more than 2 times, it will cost 100 bath for each redesign 4. customer will receive .JPG and .PNG files', 2000000003), 
-(1000000004, '1. designer will design 1 business card(both front and back) for 750 baht 2. designer will finish the work within 3 days 3. no more than 2 redesign 4. customer will receive .JPG, .PDF and .AI files', 2000000004), 
-(1000000005, '1. designer will design 1 logo for 1700 baht 2. designer will finish the work within 2 days 3. no more than 3 redesign customer will receive .JPG, .PDF and .AI files', 2000000005);
+(NULL, '1. designer will design 3 banners for 1500 baht 2. designer will finish the work within 3 days 3. no more than 3 redesign 4. customer will receive .JPG and .PNG files 5. additional 100 baht for each .AI file', 2000000002), 
+(NULL, '1. designer will design 1 banner for 600 baht 2. designer will finish the work within 2 days 3. 2 times free redesign. if redesign more than 2 times, it will cost 100 bath for each redesign 4. customer will receive .JPG and .PNG files', 2000000003), 
+(NULL, '1. designer will design 1 business card(both front and back) for 750 baht 2. designer will finish the work within 3 days 3. no more than 2 redesign 4. customer will receive .JPG, .PDF and .AI files', 2000000004), 
+(NULL, '1. designer will design 1 logo for 1700 baht 2. designer will finish the work within 2 days 3. no more than 3 redesign customer will receive .JPG, .PDF and .AI files', 2000000005);
 
 INSERT INTO User VALUES
 (1234567891234, 'somsak', 's1223ssd'), 
@@ -34,27 +34,27 @@ INSERT INTO Admin VALUES
 
 INSERT INTO Advertisement VALUES
 (4000000001, 1234567891244, '10/11/20', '10/1/21', 'lazada@gmail.com', 'BLOB of a1.png'), 
-(4000000002, 1234567891244, '11/11/20', '11/2/21', 'lazada@gmail.com', 'BLOB of a2.png'), 
-(4000000003, 1234567891245, '12/11/20', '12/2/21', 'shoppee@hotmail.com', 'BLOB of a3.png'), 
-(4000000004, 1234567891245, '13/11/20', '13/1/21', 'shoppee@hotmail.com', 'BLOB of a4.png'), 
-(4000000005, 1234567891245, '14/11/20', '14/2/21', 'adthailand@gmail.com', 'BLOB of a5.png');
+(NULL, 1234567891244, '11/11/20', '11/2/21', 'lazada@gmail.com', 'BLOB of a2.png'), 
+(NULL, 1234567891245, '12/11/20', '12/2/21', 'shoppee@hotmail.com', 'BLOB of a3.png'), 
+(NULL, 1234567891245, '13/11/20', '13/1/21', 'shoppee@hotmail.com', 'BLOB of a4.png'), 
+(NULL, 1234567891245, '14/11/20', '14/2/21', 'adthailand@gmail.com', 'BLOB of a5.png');
 
 INSERT INTO Transaction (tid, bank, amount, status) VALUES
 (5000000001, 'Government Savings Bank', 600, false), 
-(5000000003, 'CIMB', 1000, false), 
-(5000000004, 'Thanachart ', 750, false), 
-(5000000005, 'Krung Thai', 1500, false);
+(NULL, 'CIMB', 1000, false), 
+(NULL, 'Thanachart ', 750, false), 
+(NULL, 'Krung Thai', 1500, false);
 INSERT INTO Transaction VALUES
-(5000000002, 'Krungsri', 1700, true, 'mobile banking payment');
+(NULL, 'Krungsri', 1700, true, 'mobile banking payment');
 
 INSERT INTO Matchs (mid, did, cid, tid) VALUES
 (2000000001, 1234567891243, 1234567891234, 5000000003), 
-(2000000002, 1234567891238, 1234567891242, 5000000005), 
-(2000000003, 1234567891236, 1234567891235, 5000000001), 
-(2000000004, 1234567891241, 1234567891240, 5000000004);
+(NULL, 1234567891238, 1234567891242, 5000000005), 
+(NULL, 1234567891236, 1234567891235, 5000000001), 
+(NULL, 1234567891241, 1234567891240, 5000000004);
 INSERT INTO Matchs VALUES
-(2000000005, 1234567891237, 1234567891239, 5000000002, 9.8);
+(NULL, 1234567891237, 1234567891239, 5000000002, 9.8);
 
 INSERT INTO JobPosting VALUES
-(6000000001, 1234567891236, 'BLOB of j1.png', '9 years of design experience, the best quality at an affordable price. Feel free to send me a message about your desire banner :D'), 
-(6000000002, 1234567891243, 'BLOB of j2.png', 'Are you looking for an dedicated ads designer with direct experience in this field? Price is negotiable.');
+(NULL, 1234567891236, 'BLOB of j1.png', '9 years of design experience, the best quality at an affordable price. Feel free to send me a message about your desire banner :D'), 
+(NULL, 1234567891243, 'BLOB of j2.png', 'Are you looking for an dedicated ads designer with direct experience in this field? Price is negotiable.');
