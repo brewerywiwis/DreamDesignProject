71b4745e75977479836106f07c0d7d8c3035c95a
brewerywiwis <brewder136@hotmail.com>
2020-11-18 14:23:36 +0700

    update query user

--- app/DAL/manipulation.py
+++ app/DAL/manipulation.py
@@ -1,26 +1,27 @@
 def queryUser(db):
     cursor = db.cursor()
 
-    query = ("SELECT * FROM User")
+    query = ("SELECT * FROM user")
 
     # hire_start = datetime.date(1999, 1, 1)
     # hire_end = datetime.date(1999, 12, 31)
 
     cursor.execute(query)
-
+    result = list(cursor)
     # for (first_name, last_name, hire_date) in cursor:
     #     print("{}, {} was hired on {:%d %b %Y}".format(
     #         last_name, first_name, hire_date))
-    for i in cursor:
-        print(i)
+    # for i in cursor:
+    #     print(i)
 
     cursor.close()
+    return result
 
 
 def insertUser(db, name, pw):
     cursor = db.cursor()
 
-    sql = ("INSERT INTO User (username, password) VALUES (%s,%s);")
+    sql = ("INSERT INTO user (username, password) VALUES (%s,%s);")
 
     cursor.execute(sql, (name, pw))
 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import Label
 import DAL.connection as connection
 import DAL.manipulation as manipulation
 
@@ -9,7 +10,8 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        # self.pack()
+        self.grid()
         self.create_widgets()
         self.create_menu()
 
@@ -17,21 +19,25 @@ class Application(tk.Frame):
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.hi_there.grid(row=0, column=0)
+        # self.hi_there.pack(side="top")
 
         self.getUser = tk.Button(self)
         self.getUser["text"] = "click for \nget user list"
         self.getUser["command"] = self.queryUser
-        self.getUser.pack(side="top")
+        self.getUser.grid(row=0, column=1)
+        # self.getUser.pack(side="top")
 
-        self.getUser = tk.Button(self)
-        self.getUser["text"] = "click for \ninsert user"
-        self.getUser["command"] = self.insertUser
-        self.getUser.pack(side="top")
+        self.inUser = tk.Button(self)
+        self.inUser["text"] = "click for \ninsert user"
+        self.inUser["command"] = self.insertUser
+        self.inUser.grid(row=0, column=2)
+        # self.inUser.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.grid(row=0, column=3)
+        # self.quit.pack(side="bottom")
 
     def say_hi(self):
         print("hi there, everyone!")
@@ -51,10 +57,13 @@ class Application(tk.Frame):
     def donothing(self):
         filewin = tk.Toplevel(self.master)
         button = tk.Button(filewin, text="Do nothing button")
-        button.pack()
+        # button.pack()
 
     def queryUser(self):
-        manipulation.queryUser(db.getDB())
+        result = manipulation.queryUser(db.getDB())
+        for i, k in enumerate(result):
+            label = Label(self.master, text=k)
+            label.grid(column=0, row=i+1, sticky="W")
 
     def insertUser(self):
         manipulation.insertUser(db.getDB(), "AAA", "4321")
