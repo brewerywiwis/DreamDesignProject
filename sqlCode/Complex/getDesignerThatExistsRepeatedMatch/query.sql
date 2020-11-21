8833d0e7e4fdf69d99193c8e8e91e4f13823931b
Chayapat Temudomsomboon <chayapatlbs@gmail.com>
2020-11-22 00:51:07 +0700

    Opzimized Complex Query2

--- sqlCode/Complex/getDesignerThatExistsRepeatedMatch/query.sql
+++ sqlCode/Complex/getDesignerThatExistsRepeatedMatch/query.sql
@@ -3,10 +3,10 @@
 SELECT D2.uid , D2.reviewScore
 FROM designer D2
 WHERE EXISTS
-	(SELECT D.uid
-	FROM matchs M , designer D , customer C
-	WHERE C.uid = M.cid AND D.uid = M.did AND D2.uid = D.uid
-	GROUP BY D.uid , C.uid
-	HAVING count(M.mid) >= 1)
+	(SELECT M.did
+	FROM matchs M 
+	WHERE D2.uid = M.did
+	GROUP BY M.cid , M.did
+	HAVING count(M.mid) > 1)
 ORDER BY D2.reviewScore DESC
 LIMIT 5;
